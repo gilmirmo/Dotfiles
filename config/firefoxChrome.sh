7685b5712d359aa65ec788d415ce7c02175d8fe5
Amir Mohammad Gilani <amir.mg97@gmail.com>
2019-02-02 13:49:00 +0100

    doots-rice

---
+++ config/firefoxChrome.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cat ~/.cache/wal/colors.css ~/.config/firefoxTheme.css > ~/.mozilla/firefox/x17mhv47.default/chrome/userChrome.css
---
+++ config/ui-update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wal -i ~/Pictures/Wallpapers/wallhaven.jpg
+
+~/.config/polybar/polybar.sh &
+~/.config/dunst/dunst.sh &
+~/.config/firefoxChrome.sh
